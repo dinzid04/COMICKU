e350297f3dc79b34a7ba5b7ebd7158f5fd817af2
xvinn_voc <gadingkencana04@gmail.com>
2025-10-21 05:35:17 +0700

    Update firebaseConfig.ts

--- client/src/firebaseConfig.ts
+++ client/src/firebaseConfig.ts
@@ -5,13 +5,13 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "MOCK_API_KEY",
-  authDomain: "MOCK_AUTH_DOMAIN",
-  projectId: "MOCK_PROJECT_ID",
-  storageBucket: "MOCK_STORAGE_BUCKET",
-  messagingSenderId: "MOCK_SENDER_ID",
-  appId: "MOCK_APP_ID"
-};
+    apiKey: "AIzaSyAQpLgRcRkhJcHtJrkX6KktoAYEknUj-GA",
+    authDomain: "comicku-a4aae.firebaseapp.com",
+    projectId: "comicku-a4aae",
+    storageBucket: "comicku-a4aae.firebasestorage.app",
+    messagingSenderId: "449222490773",
+    appId: "1:449222490773:web:2038082d8995a70d11b99c"
+  };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
