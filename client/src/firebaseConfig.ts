40f60e44c6e2514ff82f607760c2d0a17aeba87c
xvinn_voc <gadingkencana04@gmail.com>
2025-10-21 08:51:57 +0700

    Update firebaseConfig.ts

--- client/src/firebaseConfig.ts
+++ client/src/firebaseConfig.ts
@@ -9,14 +9,14 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID",
-  measurementId: "YOUR_MEASUREMENT_ID"
-};
+    apiKey: "AIzaSyAQpLgRcRkhJcHtJrkX6KktoAYEknUj-GA",
+    authDomain: "comicku-a4aae.firebaseapp.com",
+    projectId: "comicku-a4aae",
+    storageBucket: "comicku-a4aae.firebasestorage.app",
+    messagingSenderId: "449222490773",
+    appId: "1:449222490773:web:2038082d8995a70d11b99c"
+  };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
